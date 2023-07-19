3856d8c758241db5d05e96e0db04c3ec6bca0b07
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 23:02:11 +0800

    Update ssrplus.sh

--- ssrplus.sh
+++ ssrplus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk luci-app-ssr-plus
 #mv -f helloworld/* ./
 #rm -rf helloworld
 
