bad5cb4fff176abbb9f4e8b1fc3c41076e0b494a
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 22:59:50 +0800

    Update ssrplus.sh

--- ssrplus.sh
+++ ssrplus.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-#svn co https://github.com/fw876/helloworld luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus luci-app-ssr-plus
+#mv -f helloworld/* ./
+#rm -rf helloworld
+
+
+# 生成完整目录清单
+cat > Update.txt <<EOF
+UpdateList.txt
+luci-app-ssr-plus
+naiveproxy
+shadowsocks-rust
+shadowsocksr-libev
+simple-obfs
+tcping
+trojan
+v2ray-core
+v2ray-geodata
+v2ray-plugin
+v2raya
+xray-core
+xray-plugin
+EOF
+
+exit 0
