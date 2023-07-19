3e5bd0216759ce20c8b08e4a059518cdef878dc0
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 16:58:57 +0800

    Update ssrplus.sh

--- ssrplus.sh
+++ ssrplus.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/fw876/helloworld.git
-mv -f helloworld/* ./
-rm -rf helloworld
-
-
-# 生成完整目录清单
-cat > Update.txt <<EOF
-UpdateList.txt
-luci-app-ssr-plus
-naiveproxy
-shadowsocks-rust
-shadowsocksr-libev
-simple-obfs
-tcping
-trojan
-v2ray-core
-v2ray-geodata
-v2ray-plugin
-v2raya
-xray-core
-xray-plugin
-EOF
-
-exit 0
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall passwall
