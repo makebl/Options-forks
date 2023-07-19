b0463d209992b3244902c179d5cd5ecfac1f342d
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 16:59:37 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -1,42 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git
 
-svn co https://github.com/xiaorouji/trunk/openwrt-passwall passwall
-
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall passwall
   
-fi
-mv -f passwall/* ./
-rm -rf passwall
 
-
-# 生成完整目录清单
-cat > Update.txt <<EOF
-UpdateList.txt
-brook
-chinadns-ng
-dns2socks
-hysteria
-ipt2socks
-luci-app-passwall
-luci-app-passwall2
-microsocks
-naiveproxy
-pdnsd-alt
-shadowsocks-rust
-shadowsocksr-libev
-simple-obfs
-ssocks
-tcping
-trojan
-trojan-go
-trojan-plus
-v2ray-core
-v2ray-geodata
-v2ray-plugin
-xray-core
-xray-plugin
-EOF
 
 #TG通知
 if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🚫插件源码同步失败，分支：Package_$matrix_target，失败列表：$FOLDERSX......"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉passwall插件源码同步成功，仓库passwall.git......"; fi
