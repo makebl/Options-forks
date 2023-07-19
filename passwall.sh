c3690e2da517489a286384c965f1d4bdc00e2e06
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 21:08:42 +0800

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -7,7 +7,7 @@ curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/a
 
 svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 luci-app-passwall2
 
-svn co https://github.com/xiaorouji/openwrt-passwall/packages relevance
+git clone https://github.com/xiaorouji/openwrt-passwall/packages relevance
  
 
 
