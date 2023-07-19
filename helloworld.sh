c828116b047290fc0031623d3c14e2ca499c925d
马可 <78850005+makebl@users.noreply.github.com>
2023-07-19 23:15:20 +0800

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#svn co https://github.com/fw876/helloworld/trunk luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk relevance
 #mv -f helloworld/* ./
-#rm -rf helloworld
+rm -rf luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus luci-app-ssr-plus
 
 # 生成完整目录清单
