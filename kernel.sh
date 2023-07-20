2c6e0828116fbaa856130ea87cf54c36679614da
马可 <78850005+makebl@users.noreply.github.com>
2023-07-20 12:23:22 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -2,13 +2,11 @@
 
 
 
-svn co https://github.com/danshui-git/amlogic-s9xxx-openwrt/trunk/amlogic-s9xxx/amlogic-kernel kernel
+git clone https://github.com/281677160/openwrt-package.git
+cd openwrt-package
+git checkout Lede/relevance
+cd openwrt-package
+git checkout Lede/relevance
+cd openwrt-package
+git pull origin Lede/relevance
 
-
-
-# 删除拉取插件后残留的.git和.svn,再随带删除各种README说明
-find ./*/ -name '.git' | xargs -i rm -rf {}
-find . -name '.svn' | xargs -i rm -rf {}
-find . -name '*.md' -o -name 'LICENSE' | xargs -i rm -rf {}
-find . -name '.gitattributes' -o -name '.gitignore' | xargs -i rm -rf {}
-exit 0
