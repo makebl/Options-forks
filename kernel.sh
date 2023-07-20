4cfff654eb83dc3b5f936da3e9e3e9997740fb95
马可 <78850005+makebl@users.noreply.github.com>
2023-07-20 12:29:19 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -3,7 +3,7 @@
 
 
 git clone https://github.com/281677160/openwrt-package.git
-cd openwrt-package
+cd openwrt-package/relevance
 git checkout Lede/relevance
 cd openwrt-package
 git checkout Lede/relevance
