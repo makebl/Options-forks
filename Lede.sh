8e3d60e8bd15ffd2489bbd7f31ee3b904abcb1e9
马可 <78850005+makebl@users.noreply.github.com>
2023-07-20 14:22:11 +0800

    Update Lede.sh

--- Lede.sh
+++ Lede.sh
@@ -226,12 +226,12 @@ svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-v2ray-se
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk lua-maxminddb
 #svn co https://github.com/vernesong/OpenClash/trunk luci-app-openclash
 #rm -rf luci-app-openclash/img
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall luci-app-passwall
-mkdir -p luci-app-passwall/root/usr/share/passwall/rules/direct_host
-curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/direct_host > luci-app-passwall/root/usr/share/passwall/rules/direct_host
+#svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall luci-app-passwall
+#mkdir -p luci-app-passwall/root/usr/share/passwall/rules/direct_host
+#curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/direct_host > luci-app-passwall/root/usr/share/passwall/rules/direct_host
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-clash luci-app-clash
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 luci-app-passwall2
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus luci-app-ssr-plus
+#svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 luci-app-passwall2
+#svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus luci-app-ssr-plus
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-bypass luci-app-bypass
 svn co https://github.com/shidahuilang/OpenClash/trunk luci-app-xiaolaoshu
 rm -rf luci-app-xiaolaoshu/img
