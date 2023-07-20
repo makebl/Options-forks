026a8b716bef533cf18693f7ffa0f9b2a233e50d
马可 <78850005+makebl@users.noreply.github.com>
2023-07-20 15:19:19 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+# https://github.com/shidahuilang/openwrt
+# common Module by dahuilang
 
-
-
-svn co https://github.com/281677160/openwrt-package/trunk/relevance relevance
-
+svn co https://github.com/kenzok8/small/trunk/luci-app-bypass luci-app-bypass
