4ef7d1e82715dd37115505ed365dfac0e350ba67
马可 <78850005+makebl@users.noreply.github.com>
2023-07-20 13:26:24 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn co https://github.com/281677160/openwrt-package.gut;package
+svn co https://github.com/281677160/openwrt-package.git;package
 
 svn co https://github.com/281677160/openwrt-package/trunk/relevance relevance
 
