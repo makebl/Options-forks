a204870e7de9e580be55287c4c0fa470dca7eccc
马可 <78850005+makebl@users.noreply.github.com>
2023-07-26 22:04:49 +0800

    Update Lede.sh

--- Lede.sh
+++ Lede.sh
@@ -107,15 +107,15 @@ git clone https://github.com/shidahuilang/OpenClash luci-app-xiaolaoshu
 rm -rf luci-app-xiaolaoshu/img
 # git clone https://github.com/sirpdboy/luci-app-netdata luci-app-netdata
 
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall luci-app-passwall
-mkdir -p luci-app-passwall/root/usr/share/passwall/rules/direct_host
-curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/direct_host > luci-app-passwall/root/usr/share/passwall/rules/direct_host
+#svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall luci-app-passwall
+#mkdir -p luci-app-passwall/root/usr/share/passwall/rules/direct_host
+#curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/direct_host > luci-app-passwall/root/usr/share/passwall/rules/direct_host
 
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash luci-app-openclash
-mkdir -p luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
-curl -fsSL  https://raw.githubusercontent.com/makebl/openwrt-package/usb/argon/openclash_custom_rules.list > luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
-sed -i '/openclash.config.enable/{N;d;}' luci-app-openclash/root/etc/uci-defaults/luci-openclash #OpenClash恢复更新系统开机自启动
-rm -rf luci-app-openclash/img
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash luci-app-openclash
+#mkdir -p luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+#curl -fsSL  https://raw.githubusercontent.com/makebl/openwrt-package/usb/argon/openclash_custom_rules.list > luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+#sed -i '/openclash.config.enable/{N;d;}' luci-app-openclash/root/etc/uci-defaults/luci-openclash #OpenClash恢复更新系统开机自启动
+#rm -rf luci-app-openclash/img
 
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git chinadns-ng
 git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git luci-app-chinadns-ng
