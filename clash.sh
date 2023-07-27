8e9c9043813fd9ba3541a839906564c5a5f5505f
马可 <78850005+makebl@users.noreply.github.com>
2023-07-27 20:29:34 +0800

    Update clash.sh

--- clash.sh
+++ clash.sh
@@ -2,5 +2,5 @@
 # https://github.com/shidahuilang/openwrt
 # common Module by dahuilang
 
-# git clone https://github.com/vernesong/OpenClash.git
-git clone master --depth 1 https://github.com/vernesong/OpenClash.git
+git clone https://github.com/vernesong/OpenClash.git
+
