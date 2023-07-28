a4881631ef65c939971ca442d0796d31243abc5f
马可 <78850005+makebl@users.noreply.github.com>
2023-07-28 12:25:20 +0800

    Update master1.sh

--- master1.sh
+++ master1.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash luci-app-openclash   
+git clone -b master --depth 1 https://github.com/vernesong/OpenClash luci-app-openclash
+mkdir -p luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/openclash_custom_rules.list > luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+sed -i '/openclash.config.enable/{N;d;}' luci-app-openclash/root/etc/uci-defaults/luci-openclash #OpenClash恢复更新系统开机自启动
+
+rm -rf luci-app-openclash/img
+rm -rf luci-app-openclash/version
+rm -rf luci-app-openclash/core_version
