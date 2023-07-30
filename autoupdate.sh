27e1a6590835bab0544f2bea96e8f7086bb5702d
马可 <78850005+makebl@users.noreply.github.com>
2023-07-30 11:34:25 +0800

    Create autoupdate.sh

---
+++ autoupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## https://github.com/shidahuilang/OpenWrt
+## common Module by shidahuilang
+
+
+
+git clone -b main --depth 1 https://github.com/281677160/luci-app-autoupdate luci-app-autoupdate
