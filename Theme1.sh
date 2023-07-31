c911fdbc79f0d650449be81e8d72124ca02dc855
马可 <78850005+makebl@users.noreply.github.com>
2023-07-31 12:17:08 +0800

    Update Theme1.sh

--- Theme1.sh
+++ Theme1.sh
@@ -3,8 +3,10 @@
 # common Module by shidahuilang
 
 # 主题
-git clone -b 18.06 https://github.com/281677160/luci-theme-argon luci-theme-argon
-git clone -b argon-config https://github.com/281677160/luci-theme-argon luci-app-argon-config
+# git clone -b 18.06 https://github.com/281677160/luci-theme-argon luci-theme-argon
+# git clone -b argon-config https://github.com/281677160/luci-theme-argon luci-app-argon-config
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-theme-argon luci-theme-argon
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-app-argon-config luci-app-argon-config
 git clone https://github.com/gngpp/luci-theme-design.git
 git clone https://github.com/gngpp/luci-app-design-config.git
 git clone https://github.com/sirpdboy/luci-theme-opentopd
@@ -29,12 +31,21 @@ git clone -b LUCI-LUA-UCITRACK https://github.com/281677160/feed-netkeeper  && m
 sed -i 's/bypass/shadowsocksr/g' luci-theme-kucat/luasrc/view/themes/kucat/header.htm
 cp -Rf ${GITHUB_WORKSPACE}/README/style.css luci-theme-kucat/htdocs/luci-static/kucat/css/style.css
 
+# 主题
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/themes/luci-theme-opentomcat luci-theme-opentomcat
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-theme-opentomato luci-theme-opentomato
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-theme-neobird luci-theme-neobird
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-theme-neobird2 luci-theme-neobird2
 
 ## 生成完整目录清单
 cat > Update.txt <<EOF
 feed-netkeeper
 luci-app-argon-config
 luci-app-design-config
+luci-theme-neobird2
+luci-theme-neobird
+luci-theme-opentomato
+luci-theme-opentomcat
 luci-app-netkeeper-interception
 luci-app-smartdns
 luci-proto-netkeeper
