d61f91d658f8fc2cc6c46af641bf9d0de8ab465b
马可 <78850005+makebl@users.noreply.github.com>
2023-08-01 00:30:01 +0800

    Update token.sh

--- token.sh
+++ token.sh
@@ -88,11 +88,15 @@ do
 done
 
 #TG通知
-if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🚫插件源码同步失败，分支：Package_$matrix_target，失败列表：$FOLDERSX......"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉openwrt插件源码同步成功，分支：Package_$matrix_target......"; fi
+#if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🚫插件源码同步失败，分支：Package_$matrix_target，失败列表：$FOLDERSX......"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉openwrt插件源码同步成功，分支：Package_$matrix_target......"; fi
+
+# 判断变量值，如果有效发送微信通知
+#if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=$FOLDERSX--同步失败; fi
+#TG通知
+if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot${{ secrets.TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{ secrets.TELEGRAM_CHAT_ID }}&text=🚫插件源码同步失败，分支：${REPO_BRANCH}Package_${{ matrix.target }}，失败列表：$FOLDERSX......"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=${{ secrets.TELEGRAM_CHAT_ID }}&text=🎉openwrt插件源码同步成功，分支：${REPO_BRANCH}Package_${{ matrix.target }}......"; fi
 
 # 判断变量值，如果有效发送微信通知
 if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=$FOLDERSX--同步失败; fi
-
 exit 0
 
 # 删除对比更新目录列表
