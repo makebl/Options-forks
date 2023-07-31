0785cade3cce5cebc97bb4631feb4c0e72d09b82
马可 <78850005+makebl@users.noreply.github.com>
2023-07-31 12:18:57 +0800

    Update Theme2.sh

--- Theme2.sh
+++ Theme2.sh
@@ -16,11 +16,21 @@ git clone https://github.com/pymumu/openwrt-smartdns smartdns
 git clone -b master https://github.com/281677160/feed-netkeeper  && mv feed-netkeeper/* ./
 
 
+# 主题
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/themes/luci-theme-opentomcat luci-theme-opentomcat
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-theme-opentomato luci-theme-opentomato
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-theme-neobird luci-theme-neobird
+svn co https://github.com/shidahuilang/luci-theme/trunk/luci-theme-neobird2 luci-theme-neobird2
+
 ## 生成完整目录清单
 cat > Update.txt <<EOF
 feed-netkeeper
 luci-app-argon-config
 luci-app-design-config
+luci-theme-neobird2
+luci-theme-neobird
+luci-theme-opentomcat
+luci-theme-opentomato
 luci-app-netkeeper-interception
 luci-app-smartdns
 luci-proto-netkeeper
