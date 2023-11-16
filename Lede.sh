c1f93dcdad361a6576e7365aac528bb281333e47
马可 <78850005+makebl@users.noreply.github.com>
2023-11-16 19:25:19 +0800

    Update Lede.sh

--- Lede.sh
+++ Lede.sh
@@ -144,6 +144,19 @@ svn co https://github.com/kenzok8/small-package/trunk/gn ./gn
 svn co https://github.com/kenzok8/small-package/trunk/dns2tcp ./dns2tcp
 svn co https://github.com/kenzok8/small-package/trunk/dns2socks ./dns2socks
 svn co https://github.com/kenzok8/small-package/trunk/lua-neturl ./lua-neturl
+#!/bin/sh
+
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash luci-app-openclash
+# svn export https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash luci-app-openclash
+
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash luci-app-openclash
+mkdir -p luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+curl -fsSL  https://raw.githubusercontent.com/shidahuilang/openwrt-package/usb/argon/openclash_custom_rules.list > luci-app-openclash/root/etc/openclash/custom/openclash_custom_rules.list
+sed -i '/openclash.config.enable/{N;d;}' luci-app-openclash/root/etc/uci-defaults/luci-openclash #OpenClash恢复更新系统开机自启动
+
+#rm -rf luci-app-openclash/img
+#rm -rf luci-app-openclash/version
+#rm -rf luci-app-openclash/core_version
 
 # 依赖
 svn co https://github.com/kenzok8/jell/trunk/go-aliyundrive-webdav ./go-aliyundrive-webdav
