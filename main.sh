31fc553903c33e8913d93e3b918bf4f951593f38
马可 <78850005+makebl@users.noreply.github.com>
2023-11-21 16:08:53 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ## https://github.com/shidahuilang/OpenWrt
-## common Module by shidahuilang
 
-svn export https://github.com/JuvenileQ/delete-workflow-runs.git
+
+git clone https://github.com/JuvenileQ/delete-workflow-runs.git
